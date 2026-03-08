9ad5e92f397949a867c2f17d57f966697318940b
mast1f0 <avorobev602@gmail.com>
2026-03-08 15:33:27 +0300

    broken

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"market/internal/adapters/storage/memory"
+	"market/internal/adapters/storage/orm"
 	"market/internal/http"
 	"market/internal/http/handlers"
 	"market/internal/service"
 )
 
 func main() {
-	repo := memory.NewMemory()
+	repo := orm.NewStorage()
 
 	productService := service.NewProductService(repo)
+	//categoryService := service.NewCategoryService(repo)
 	productHandler := handlers.NewProductHandler(productService)
+	//categoryHandler := handlers.NewCategoryHandler(categoryService)
 	router := http.SetupRoutes(productHandler)
 	srv := http.NewServer(router)
 	srv.Start()
